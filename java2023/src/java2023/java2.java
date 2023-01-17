de9a19c7a420d210f101b99559d587a7c545291d
Daniela-ribeiro <danielagomesk1999@gmail.com>
2023-01-17 19:00:39 -0300

    Enviar java2

---
+++ java2023/src/java2023/java2.java
@@ -0,0 +1,26 @@
+package java2023;
+
+public class java2 {
+
+	public static void main(String[] args) {
+		int numero1 = 3;
+		int numero2 = 7;
+		
+		if (numero1 == numero2)	
+		System.out.println(" O numéro " + numero1 + " é igual ao numéro " + numero2);
+		
+		if (numero1 != numero2)	
+		System.out.println(" O numéro " + numero1 + " é diferente do numéro " + numero2);
+		
+		if (numero1 > numero2)	
+		System.out.println(" O numéro " + numero1 + " é maior que o numéro " + numero2);
+		
+		if (numero1 < numero2)
+			System.out.println(" O numéro " + numero1 + " é menor que o numéro " + numero2);
+			
+		
+		
+
+	}
+
+}
